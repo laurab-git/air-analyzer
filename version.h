ef6caa56925dd140188d9716bd9efe582685ff45
Laurent Rabret <laurent.rabret@orange.com>
2026-03-10 20:23:39 +0100

    test: nettoyage fichier test

--- version.h
+++ version.h
@@ -4,9 +4,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define BUILD_NUMBER 7
-#define GIT_HASH "35a2ac8"
+#define BUILD_NUMBER 8
+#define GIT_HASH "00ddc2a"
 #define BUILD_DATE "2026-03-10 20:23:39"
-#define FIRMWARE_VERSION "v7"
+#define FIRMWARE_VERSION "v8"
 
 #endif
